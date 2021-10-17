7183d517d1fce51f449a6979877058ba45b59293
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2021-10-16 20:29:16 -0700

    Added form validation with formik and yup

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react"
 import ReCAPTCHA from "react-google-recaptcha"
 import Media from "react-media"
+import { Formik } from "formik"
+import * as yup from "yup"
 import {
   Container,
   Row,
@@ -29,17 +31,15 @@ const Contact = ({ isMobile }) => {
   const [formFailure, setFormFailure] = useState(false)
   const recaptchaRef = useRef()
   const [submissionContent, setSubmissionConent] = useState({})
-  // const [isMobile, setIsMobile] = useState(true)
 
-  const handleChange = event => {
+  const onChange = event => {
     setSubmissionConent({
       ...submissionContent,
       [event.target.name]: event.target.value,
     })
   }
 
-  const handleSubmit = event => {
-    event.preventDefault()
+  const netlifySubmit = values => {
     setFormFailure(false)
     const recaptchaValue = recaptchaRef.current.getValue()
 
@@ -47,8 +47,9 @@ const Contact = ({ isMobile }) => {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        "form-name": event.target.getAttribute("name"),
+        "form-name": "contact",
         ...submissionContent,
+        ...values,
         "g-recaptcha-response": recaptchaValue,
       }),
     })
@@ -56,20 +57,28 @@ const Contact = ({ isMobile }) => {
         if (response.status === 200 && !response.redirected) {
           setFormSuccess(true)
         } else {
-          console.log("Error occured:")
-          console.log(response.json())
           setFormFailure(true)
         }
       })
       .catch(error => {
         setFormFailure(true)
-        console.log(error)
       })
   }
 
+  const contactSchema = yup.object({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("A valid email must be entered")
+      .required("Email address is required"),
+    message: yup
+      .string()
+      .max(2200, "The message max length is 2200 characters")
+      .required("A message is required"),
+  })
+
   return (
     <Container fluid className="pt-3">
-      {/* Section row */}
       <Row className={isMobile ? "px-1" : "px-5"}>
         {/* Image Column */}
         <Col lg={true} className="d-flex flex-column pt-3">
@@ -87,108 +96,154 @@ const Contact = ({ isMobile }) => {
           <h1 className="text-center">Send Me An Email!</h1>
 
           <Card className="mt-auto">
-            <Form
-              className="px-3 py-3"
-              name="contact"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              data-netlify-recaptcha="true"
-              onSubmit={handleSubmit}
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                message: "",
+              }}
+              validationSchema={contactSchema}
+              onSubmit={netlifySubmit}
             >
-              <input type="hidden" name="form-name" value="contact" />
-              <p hidden>
-                <label>
-                  Don't fill this out:{" "}
-                  <input name="bot-field" onChange={handleChange} />
-                </label>
-              </p>
+              {({
+                handleSubmit,
+                handleChange,
+                values,
+                touched,
+                isValid,
+                errors,
+              }) => (
+                <Form
+                  className="px-3 py-3"
+                  name="contact"
+                  method="post"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                  data-netlify-recaptcha="true"
+                  noValidate
+                  onSubmit={handleSubmit}
+                >
+                  <input type="hidden" name="form-name" value="contact" />
+                  <p hidden>
+                    <label>
+                      Don't fill this out:{" "}
+                      <input name="bot-field" onChange={onChange} />
+                    </label>
+                  </p>
 
-              {formSuccess === false ? (
-                <></>
-              ) : (
-                <Alert variant="success">
-                  <Alert.Heading>Thank you!</Alert.Heading>I will respond to
-                  your email as soon as possible.
-                </Alert>
-              )}
+                  {formSuccess === true ? (
+                    <></>
+                  ) : (
+                    <Alert variant="success">
+                      <Alert.Heading>Thank you!</Alert.Heading>I will respond to
+                      your email as soon as possible! If you do not hear back
+                      within a couple days, feel free to reach out to me on
+                      LinkedIn.
+                    </Alert>
+                  )}
 
-              {formFailure === false ? (
-                <></>
-              ) : (
-                <Alert variant="danger">
-                  <Alert.Heading>Oops!</Alert.Heading>An error occurred when
-                  trying to submit your email. If this error persists, please
-                  reach out to me on LinkedIn!
-                </Alert>
-              )}
+                  {formFailure === false ? (
+                    <></>
+                  ) : (
+                    <Alert variant="danger">
+                      <Alert.Heading>Oops!</Alert.Heading>An error occurred when
+                      trying to submit your email. If this error persists,
+                      please reach out to me on LinkedIn!
+                    </Alert>
+                  )}
 
-              <Form.Group controlId="formContactName" className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="First Name Last Name"
-                  maxLength={100}
-                  name="name"
-                  onChange={handleChange}
-                ></Form.Control>
-              </Form.Group>
+                  <Form.Group controlId="formContactName" className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="First Name Last Name"
+                      maxLength={100}
+                      name="name"
+                      onChange={handleChange}
+                      value={values.name}
+                      isValid={touched.name && !errors.name}
+                      isInvalid={!!errors.name}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.name}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <Form.Group controlId="formContactEmail" className="mb-3">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="yourName@company.com"
-                  maxLength={254}
-                  name="email"
-                  onChange={handleChange}
-                ></Form.Control>
-              </Form.Group>
+                  <Form.Group controlId="formContactEmail" className="mb-3">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="yourName@company.com"
+                      maxLength={254}
+                      name="email"
+                      onChange={handleChange}
+                      value={values.email}
+                      isValid={touched.email && !errors.email}
+                      isInvalid={!!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <Form.Group controlId="formContactMessage" className="mb-3">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={4}
-                  placeholder="Hello Kyle! My name is ___, I found your portfolio and wanted to schedule an interview with you sometime this week."
-                  style={{ resize: "none" }}
-                  maxLength={2200}
-                  name="message"
-                  onChange={handleChange}
-                ></Form.Control>
-              </Form.Group>
+                  <Form.Group controlId="formContactMessage" className="mb-3">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={4}
+                      placeholder="Hello Kyle! My name is ___, I found your portfolio and wanted to schedule an interview with you sometime this week."
+                      style={{ resize: "none" }}
+                      maxLength={2200}
+                      name="message"
+                      aria-describedby="messageCharCount"
+                      onChange={handleChange}
+                      value={values.message}
+                      isValid={touched.message && !errors.message}
+                      isInvalid={!!errors.message}
+                    />
+                    <Form.Text id="messageCharCount" muted>
+                      {values.message.length}/2200 characters used
+                    </Form.Text>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.message}
+                    </Form.Control.Feedback>
+                  </Form.Group>
 
-              <Media
-                queries={{
-                  mobile: "(max-width: 599px)",
-                  desktop: "(min-width: 600px)",
-                }}
-              >
-                {matches => (
-                  <>
-                    {matches.mobile && (
-                      <ReCAPTCHA
-                        size="compact"
-                        ref={recaptchaRef}
-                        sitekey={RECAPTCHA_KEY}
-                      />
+                  <Media
+                    queries={{
+                      mobile: "(max-width: 599px)",
+                      desktop: "(min-width: 600px)",
+                    }}
+                  >
+                    {matches => (
+                      <>
+                        {matches.mobile && (
+                          <ReCAPTCHA
+                            size="compact"
+                            ref={recaptchaRef}
+                            sitekey={RECAPTCHA_KEY}
+                          />
+                        )}
+                        {matches.desktop && (
+                          <ReCAPTCHA
+                            ref={recaptchaRef}
+                            sitekey={RECAPTCHA_KEY}
+                          />
+                        )}
+                      </>
                     )}
-                    {matches.desktop && (
-                      <ReCAPTCHA ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
-                    )}
-                  </>
-                )}
-              </Media>
+                  </Media>
 
-              <Button
-                variant="outline-secondary"
-                type="submit"
-                className="w-100 mt-3"
-                disabled={true}
-              >
-                Submit
-              </Button>
-            </Form>
+                  <Button
+                    variant="outline-secondary"
+                    type="submit"
+                    className="w-100 mt-3"
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik>
           </Card>
         </Col>
       </Row>
