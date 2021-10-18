0fc6ec392ed9b04c03af4ca396e725c7ba13e0a8
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2021-10-17 17:54:56 -0700

    Fixed zooming out UI responsiveness

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
 import * as React from "react"
 import Header from "./header"
+import { Container } from "react-bootstrap"
 import "../styles/global.css"
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Container fluid className="px-0" style={{ maxWidth: 1920 }}>
       <Header />
       <main>{children}</main>
-      {/* <Footer /> */}
-    </>
+    </Container>
   )
 }
 
